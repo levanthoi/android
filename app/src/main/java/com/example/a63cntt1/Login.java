04d5518f138b4b614823572dbf9ba7e8e6190a23
levanthoi <lethoi1506@gmail.com>
2024-03-03 17:11:20 +0700

    first commit

---
+++ app/src/main/java/com/example/a63cntt1/Login.java
@@ -0,0 +1,103 @@
+package com.example.a63cntt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button register;
+    Button login;
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    private void setupUI() {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+    }
+
+    private void setupListeners() {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUsername();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        setupUI();
+        setupListeners();
+    }
+
+    void checkUsername() {
+        boolean isValid = true;
+        if (isEmpty(username)) {
+            username.setError("You must enter username to login!");
+            isValid = false;
+        } else {
+            if (!isEmail(username)) {
+                username.setError("Enter valid email!");
+                isValid = false;
+            }
+        }
+
+        if (isEmpty(password)) {
+            password.setError("You must enter password to login!");
+            isValid = false;
+        } else {
+            if (password.getText().toString().length() < 4) {
+                password.setError("Password must be at least 4 chars long!");
+                isValid = false;
+            }
+        }
+
+        //check email and password
+        //IMPORTANT: here should be call to backend or safer function for local check; For example simple check is cool
+        //For example simple check is cool
+        if (isValid) {
+            String usernameValue = username.getText().toString();
+            String passwordValue = password.getText().toString();
+            if (usernameValue.equals("test@test.com") && passwordValue.equals("password1234")) {
+                //everything checked we open new activity
+                Intent i = new Intent(Login.this, MainActivity.class);
+                startActivity(i);
+                //we close this activity
+                this.finish();
+            } else {
+                Toast t = Toast.makeText(this, "Wrong email or password!", Toast.LENGTH_SHORT);
+                t.show();
+            }
+        }
+    }
+}
